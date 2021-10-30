b259f2c088df97a2f3617b66a637dd8310a84047
QASeleniumFan <david.juicecaboose@gmail.com>
2021-10-30 00:22:18 -0400

    intial commit of actual dummy code

---
+++ src/test/java/stepdefs/DemoDefinitions.java
@@ -0,0 +1,24 @@
+package stepdefs;
+
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class DemoDefinitions {
+    @Given("that I enter for PC Part picker in the Google search bar")
+    public void that_i_enter_for_pc_part_picker_in_the_google_search_bar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click on the System Builder Link")
+    public void i_click_on_the_system_builder_link() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should appear on the system Builder page")
+    public void i_should_appear_on_the_system_builder_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
+
